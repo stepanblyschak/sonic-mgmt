30cbe526b9f807d2e93022536c21dc7c472170d9
Stepan Blyschak <stepanb@mellanox.com>
2019-07-15 15:56:56 +0300

    [acl] start working on ACL on py.test
    Signed-off-by: Stepan Blyschak <stepanb@mellanox.com>

--- ansible/roles/test/files/acstests/acltb_test.py
+++ ansible/roles/test/files/acstests/acltb_test.py
@@ -16,9 +16,6 @@ Usage:
 '''
 from __future__ import print_function
 
-import logging
-import json
-
 import ptf
 import ptf.packet as scapy
 import ptf.testutils as testutils
@@ -26,7 +23,6 @@ import ptf.testutils as testutils
 from ptf.testutils import simple_tcp_packet
 from ptf.testutils import simple_udp_packet
 from ptf.testutils import simple_icmp_packet
-from ptf.testutils import dp_poll
 from ptf.testutils import send_packet
 from ptf.mask import Mask
 from ptf.base_tests import BaseTest
@@ -48,21 +44,90 @@ class AclTest(BaseTest):
         '''
         @summary: Setup for the test
         '''
+
         self.dataplane = ptf.dataplane_instance
         self.router_mac = self.test_params['router_mac']
         self.testbed_type = self.test_params['testbed_type']
-        self.tor_ports = [int(p) for p in self.test_params['tor_ports'].split(',')]
-        self.tor_ports.sort()
-        self.spine_ports = [int(p) for p in self.test_params['spine_ports'].split(',')]
-        self.spine_ports.sort()
-        self.dst_ip_tor = self.test_params['dst_ip_tor']
-        self.dst_ip_tor_forwarded = self.test_params['dst_ip_tor_forwarded']
-        self.dst_ip_tor_blocked = self.test_params['dst_ip_tor_blocked']
-        self.dst_ip_spine = self.test_params['dst_ip_spine']
-        self.dst_ip_spine_forwarded = self.test_params['dst_ip_spine_forwarded']
-        self.dst_ip_spine_blocked = self.test_params['dst_ip_spine_blocked']
+        self.src_ports = sorted([int(p) for p in self.test_params['src_ports'].split(',')])
+        self.dst_ports = sorted([int(p) for p in self.test_params['dst_ports'].split(',')])
+        self.dst_ip = self.test_params['default_dst_ip']
+        self.src_ip = self.test_params['default_src_ip']
+        self.action = self.test_params['action']
+        self.proto = self.test_params['proto'].upper()
+
+        if self.proto == 'TCP':
+            self.init_tcp_pair()
+        elif self.proto == 'UDP':
+            self.init_udp_pair()
+        elif self.proto == 'ICMP':
+            self.init_icmp_pair()
+        else:
+            raise ValueError('Protocol {} not handled in PTF test'.format(self.proto))
+
         self.current_src_port_idx = 0  # An index for choosing a port for injecting packet
-        self.test_results = []
+
+    def init_tcp_pair(self):
+        self.pkt = simple_tcp_packet(
+            eth_dst=self.router_mac,
+            eth_src=self.dataplane.get_mac(0, 0),
+            ip_src=self.src_ip,
+            ip_dst=self.dst_ip,
+            tcp_sport=0x4321,
+            tcp_dport=0x51,
+            ip_ttl=64
+        )
+
+        self.exp_pkt = simple_tcp_packet(
+            eth_dst=self.dataplane.get_mac(0, 0),
+            eth_src=self.router_mac,
+            ip_src=self.src_ip,
+            ip_dst=self.dst_ip,
+            tcp_sport=0x4321,
+            tcp_dport=0x51,
+            ip_ttl=63
+        )
+
+    def init_udp_pair(self):
+        self.pkt = simple_udp_packet(
+            eth_dst=self.router_mac,
+            eth_src=self.dataplane.get_mac(0, 0),
+            ip_src=self.src_ip,
+            ip_dst=self.dst_ip,
+            udp_sport=1234,
+            udp_dport=80,
+            ip_ttl=64
+        )
+
+        self.exp_pkt = simple_udp_packet(
+            eth_dst=self.dataplane.get_mac(0, 0),
+            eth_src=self.router_mac,
+            ip_src=self.src_ip,
+            ip_dst=self.dst_ip,
+            udp_sport=1234,
+            udp_dport=80,
+            ip_ttl=63
+        )
+
+    def init_icmp_pair(self):
+        self.pkt = simple_icmp_packet(
+            eth_dst=self.router_mac,
+            eth_src=self.dataplane.get_mac(0, 0),
+            ip_src=self.src_ip,
+            ip_dst=self.dst_ip,
+            icmp_type=8,
+            icmp_code=0,
+            ip_ttl=64
+        )
+
+        self.exp_pkt = simple_icmp_packet(
+            eth_dst=self.dataplane.get_mac(0, 0),
+            eth_src=self.router_mac,
+            ip_src=self.src_ip,
+            ip_dst=self.dst_ip,
+            icmp_type=8,
+            icmp_code=0,
+            ip_ttl=63
+        )
 
     def _select_src_port(self, src_ports):
         """
@@ -77,33 +142,6 @@ class AclTest(BaseTest):
         port = src_ports[self.current_src_port_idx]
         self.current_src_port_idx = (self.current_src_port_idx + 1) % len(src_ports)
         return port
-
-    def verify_packet_any_port(self, pkt, ports, device_number=0):
-        """
-        @summary: Check that the packet is received on _any_ of the specified ports belonging to
-        the given device (default device_number is 0).
-
-        The function returns when either the expected packet is received or timeout (1 second).
-
-        Also verifies that the packet is or received on any other ports for this
-        device, and that no other packets are received on the device (unless --relax
-        is in effect).
-
-        @param pkt : packet to verify
-        @param ports : list of ports
-        @param device_number: device number, default is 0
-
-        @return: index of the port on which the packet is received and the packet.
-        """
-        received = False
-        match_index = 0
-        (rcv_device, rcv_port, rcv_pkt, pkt_time) = dp_poll(self, device_number=device_number, exp_pkt=pkt, timeout=1)
-
-        if rcv_port in ports:
-            match_index = ports.index(rcv_port)
-            received = True
-
-        return match_index, rcv_pkt, received
 
     def runSendReceiveTest(self, pkt2send, src_ports, pkt2recv, dst_ports, pkt_expected):
         """
@@ -124,293 +162,44 @@ class AclTest(BaseTest):
 
         # Send the packet and poll on destination ports
         send_packet(self, src_port, pkt2send)
-        logging.debug("Sent packet: " + pkt2send.summary())
-        (index, rcv_pkt, received) = self.verify_packet_any_port(masked2recv, dst_ports)
-
-        logging.debug('index=%s, received=%s' % (str(index), str(received)))
-        if received:
-            logging.debug("Received packet: " + scapy.Ether(rcv_pkt).summary())
-
         if pkt_expected:
-            logging.debug('Expected packet on dst_ports')
-            passed = True if received else False
-            logging.debug('Received: ' + str(received))
+            testutils.verify_packet_any_port(self, masked2recv, dst_ports)
         else:
-            logging.debug('No packet expected on dst_ports')
-            passed = False if received else True
-            logging.debug('Received: ' + str(received))
-        logging.debug('Passed: ' + str(passed))
-        return passed
-
-    def runAclTestCase(self, test_name, *args, **kwargs):
-        """
-        @summary: Wrapper for running ACL test cases
-        @param test_name: Friendly name of the test case
-        """
-        logging.info('Run test case: ' + test_name)
-        res = self.runSendReceiveTest(*args, **kwargs)
-        logging.info('Result of "%s": %s' % (test_name, "PASSED" if res else "FAILED"))
-        self.test_results.append({"result": res, "test_name": test_name})
-
-    def runAclTests(self, dst_ip, dst_ip_forwarded, dst_ip_blocked, src_ports, dst_ports, direction):
-        """
-        @summary: Crete and send packet to verify each ACL rule
-        """
-
-        direction = ", " + direction
-
-        print("\nPort to sent packets to:")
-        print(src_ports)
-        print("Destination IP: %s" % dst_ip)
-        print("Ports to expect packet from: ")
-        print(dst_ports)
-        print("Dst IP expected to be blocked: %s " % dst_ip_blocked)
-
-        tcp_pkt0 = simple_tcp_packet(
-            eth_dst=self.router_mac,
-            eth_src=self.dataplane.get_mac(0, 0),
-            ip_src="20.0.0.1",
-            ip_dst=dst_ip,
-            tcp_sport=0x4321,
-            tcp_dport=0x51,
-            ip_ttl=64
-        )
-
-        tcp_exp_pkt0 = simple_tcp_packet(
-            eth_dst=self.dataplane.get_mac(0, 0),
-            eth_src=self.router_mac,
-            ip_src="20.0.0.1",
-            ip_dst=dst_ip,
-            tcp_sport=0x4321,
-            tcp_dport=0x51,
-            ip_ttl=63
-        )
-
-        test_name = 'Test #0 - unmatched packet - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = 'Test #1 - source IP match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.2"
-        exp_pkt['IP'].src = "20.0.0.2"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #2 - destination IP match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].dst = dst_ip_forwarded
-        exp_pkt['IP'].dst = dst_ip_forwarded
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #3 - L4 source port match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].sport = 0x120D
-        exp_pkt['TCP'].sport = 0x120D
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #4 - L4 destination port match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].dport = 0x1217
-        exp_pkt['TCP'].dport = 0x1217
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #5 - IP protocol match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].proto = 0x7E
-        exp_pkt['IP'].proto = 0x7E
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #6 - TCP flags match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].flags = 0x1B
-        exp_pkt['TCP'].flags = 0x1B
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #7 - source port range match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].sport = 0x123A
-        exp_pkt['TCP'].sport = 0x123A
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #8 - destination port range match - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].dport = 0x123B
-        exp_pkt['TCP'].dport = 0x123B
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = '# Test #9 - rules priority - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.3"
-        exp_pkt['IP'].src = "20.0.0.3"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        # Create a ICMP packet
-        icmp_pkt0 = simple_icmp_packet(
-            eth_dst=self.router_mac,
-            eth_src=self.dataplane.get_mac(0, 0),
-            ip_src="20.0.0.1",
-            ip_dst=dst_ip,
-            icmp_type=8,
-            icmp_code=0,
-            ip_ttl=64
-        )
-
-        icmp_exp_pkt0 = simple_icmp_packet(
-            eth_dst=self.dataplane.get_mac(0, 0),
-            eth_src=self.router_mac,
-            ip_src="20.0.0.1",
-            ip_dst=dst_ip,
-            icmp_type=8,
-            icmp_code=0,
-            ip_ttl=63
-        )
-
-        test_name = 'Test #10 - ICMP source IP match, IP_PROTOCOL=0x1 - forwarded' + direction
-        pkt = icmp_pkt0.copy()
-        exp_pkt = icmp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.4"
-        exp_pkt['IP'].src = "20.0.0.4"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        # Create a UDP packet
-        udp_pkt0 = simple_udp_packet(
-            eth_dst=self.router_mac,
-            eth_src=self.dataplane.get_mac(0, 0),
-            ip_src="20.0.0.1",
-            ip_dst=dst_ip,
-            udp_sport=1234,
-            udp_dport=80,
-            ip_ttl=64
-        )
-
-        udp_exp_pkt0 = simple_udp_packet(
-            eth_dst=self.dataplane.get_mac(0, 0),
-            eth_src=self.router_mac,
-            ip_src="20.0.0.1",
-            ip_dst=dst_ip,
-            udp_sport=1234,
-            udp_dport=80,
-            ip_ttl=63
-        )
-
-        test_name = 'Test #11 - UDP source IP match, IP_PROTOCOL=0x11 - forwarded' + direction
-        pkt = udp_pkt0.copy()
-        exp_pkt = udp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.4"
-        exp_pkt['IP'].src = "20.0.0.4"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        ###########################################################################
-
-        test_name = 'Test #12 - source IP match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.6"
-        exp_pkt['IP'].src = "20.0.0.6"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #13 - destination IP match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].dst = dst_ip_blocked
-        exp_pkt['IP'].dst = dst_ip_blocked
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #14 - L4 source port match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].sport = 0x1271
-        exp_pkt['TCP'].sport = 0x1271
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #15 - L4 destination port match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].dport = 0x127B
-        exp_pkt['TCP'].dport = 0x127B
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #16 - IP protocol match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].proto = 0x7F
-        exp_pkt['IP'].proto = 0x7F
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #17 - TCP flags match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].flags = 0x24
-        exp_pkt['TCP'].flags = 0x24
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #18 - source port range match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].sport = 0x129E
-        exp_pkt['TCP'].sport = 0x129E
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #19 - destination port range match - dropped' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['TCP'].dport = 0x129F
-        exp_pkt['TCP'].dport = 0x129F
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = '# Test #20 - rules priority - forwarded' + direction
-        pkt = tcp_pkt0.copy()
-        exp_pkt = tcp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.7"
-        exp_pkt['IP'].src = "20.0.0.7"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, True)
-
-        test_name = 'Test #21 - ICMP source IP match, IP_PROTOCOL=0x1 - dropped' + direction
-        pkt = icmp_pkt0.copy()
-        exp_pkt = icmp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.8"
-        exp_pkt['IP'].src = "20.0.0.8"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
-
-        test_name = 'Test #22 - UDP source IP match, IP_PROTOCOL=0x11 - dropped' + direction
-        pkt = udp_pkt0.copy()
-        exp_pkt = udp_exp_pkt0.copy()
-        pkt['IP'].src = "20.0.0.8"
-        exp_pkt['IP'].src = "20.0.0.8"
-        self.runAclTestCase(test_name, pkt, src_ports, exp_pkt, dst_ports, False)
+            testutils.verify_no_packet_any(self, masked2recv, dst_ports)
 
     def runTest(self):
-        """
-        @summary: Crete and send packet to verify each ACL rule
-        """
+        self.runSendReceiveTest(self.pkt, self.src_ports, self.exp_pkt, self.dst_ports, self.action == 'forward')
 
-        # Inject packets into TOR ports, check on SPINE ports
-        self.runAclTests(self.dst_ip_spine,
-                         self.dst_ip_spine_forwarded,
-                         self.dst_ip_spine_blocked,
-                         self.tor_ports,
-                         self.spine_ports,
-                         "tor->spine")
+class Unmatched(AclTest):
+    pass
 
-        # Inject packets into SPINE ports, check on TOR ports
-        self.runAclTests(self.dst_ip_tor,
-                         self.dst_ip_tor_forwarded,
-                         self.dst_ip_tor_blocked,
-                         self.spine_ports,
-                         self.tor_ports,
-                         "spine->tor")
+class SourceIpMatch(AclTest):
+    def setUp(self):
+        super(SourceIpMatch, self).setUp()
+        self.pkt['IP'].src = self.test_params['source_ip']
+        self.exp_pkt['IP'].src = self.test_params['source_ip']
 
-        failed_cases = filter(lambda r: not r['result'], self.test_results)
-        if len(failed_cases) == 0:
-            print('!!!! All test cases passed! !!!!')
-        assert (len(failed_cases) == 0), "TEST FAILED. Failed test cases: " + str(failed_cases)
+class DestIpMatch(AclTest):
+    def setUp(self):
+        self.pkt['IP'].dst = self.test_params['destination_ip']
+        self.exp_pkt['IP'].dst = self.test_params['destination_ip']
+
+class L4SourcePortMatch(AclTest):
+    def runTest(self):
+        self.pkt[self.proto].sport = int(self.test_params['source_port'], 0)
+        self.exp_pkt[self.proto].sport = int(self.test_params['source_port'], 0)
+
+class L4DestPortMatch(AclTest):
+    def runTest(self):
+        self.pkt[self.proto].dport = int(self.test_params['destination_port'], 0)
+        self.exp_pkt[self.proto].dport = int(self.test_params['destination_port'], 0)
+
+class IpProtocolMatch(AclTest):
+    def runTest(self):
+        self.pkt['IP'].proto = int(self.test_params['protocol'], 0)
+        self.exp_pkt['IP'].proto = int(self.test_params['protocol'], 0)
+
+class TcpFlagsMatch(AclTest):
+    def runTest(self):
+        self.pkt['TCP'].flags = int(self.test_params['flags'], 0)
+        self.exp_pkt['TCP'].flags = int(self.test_params['flags'], 0)
---
+++ tests/acl/conftest.py
@@ -0,0 +1,67 @@
+import pytest
+from ansible_host import ansible_host
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--acl-ingress',
+        action='store_true',
+        default=True,
+        help='Run only ACL ingress tests',
+    )
+    parser.addoption(
+        '--acl-egress',
+        action='store_true',
+        default=False,
+        help='Run only ACL egress tests',
+    )
+    parser.addoption(
+        '--acl-basic',
+        action='store_true',
+        default=True,
+        help='Run only ACL basic traffic test',
+    )
+    parser.addoption(
+        '--acl-incremental',
+        action='store_true',
+        help='Run only ACL incremental configuration and traffic test',
+    )
+    parser.addoption(
+        '--acl-with-reboot',
+        action='store_true',
+        help='Run only ACL traffic tests with reboot',
+    )
+    parser.addoption(
+        '--acl-with-port-toggle',
+        action='store_true',
+        help='Run only ACL traffic tests with port toggling',
+    )
+
+
+#### THESE FIXTURES BELOW A CANDIDATES TO REMOVE ####
+
+@pytest.fixture(scope='session')
+def dut(request, testbed):
+    duthostname = testbed['dut']
+    plugin = request.config.pluginmanager.getplugin("ansible")
+
+    def init_host_mgr(**kwargs):
+        return plugin.initialize(request.config, request, **kwargs)
+    return ansible_host(init_host_mgr, duthostname)
+
+@pytest.fixture(scope='session')
+def ptfhost(request, testbed):
+    ptfhostname = 'ptf-1025' # FIXME: testbed['ptf']
+    plugin = request.config.pluginmanager.getplugin("ansible")
+
+    def init_host_mgr(**kwargs):
+        return plugin.initialize(request.config, request, **kwargs)
+
+    hostobj = ansible_host(init_host_mgr, ptfhostname)
+
+    # additionally deploy ptf tests
+    hostobj.copy(src='ptftests', dest='~/')
+    hostobj.copy(src='acstests', dest='~/')
+
+    return hostobj
+
